da9ed9321335a284d8e0c587939410bf6744a1a3
Jens Jovellano <jensjovellano@gmail.com>
2023-08-25 20:45:08 +0800

    Refactor: format 1 line

--- src/main/java/com/afs/restapi/service/mappers/CompanyMapper.java
+++ src/main/java/com/afs/restapi/service/mappers/CompanyMapper.java
@@ -16,7 +16,6 @@ public class CompanyMapper {
     public static CompanyResponse toResponse(Company company) {
         CompanyResponse companyResponse = new CompanyResponse();
         BeanUtils.copyProperties(company, companyResponse);
-
         var employees = company.getEmployees();
         companyResponse.setEmployeesCount((employees == null) ? 0 : employees.size());
         return companyResponse;
