b1674ee3792d607c1e8fc5a70c14483a1b593456
Jens Jovellano <jensjovellano@gmail.com>
2023-08-25 18:36:36 +0800

    Feat: Added Id in Company Response that was missing, along with setters and getters.

--- src/main/java/com/afs/restapi/service/dto/CompanyResponse.java
+++ src/main/java/com/afs/restapi/service/dto/CompanyResponse.java
@@ -1,10 +1,20 @@
 package com.afs.restapi.service.dto;
 
 public class CompanyResponse {
+
+    private Long id;
     private String name;
     private Integer employeesCount;
 
     public CompanyResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
